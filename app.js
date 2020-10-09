afec4965a819d267d374d5ae92849a0e5fd29e17
Ruslan <deadstone@list.ru>
2020-10-09 17:09:18 +0300

    Новые зависимости

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const PORT = process.env.PORT || 3000
 
-app.listen(3000)
+const start = async () => {
+  try {
+    const url = `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+    mongoose.set('useUnifiedTopology', true); // The new parser
+    await mongoose.connect(url, {useNewUrlParser: true})
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+  } catch (e) {
+    console.log('Ошибка подключения к базе данных', e );
+  }
+}
+
+start();
